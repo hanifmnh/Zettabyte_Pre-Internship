ad8280d2f349ef97ab6a13cbb4ace9eb02a379c3
Nur Hanif <hanifmn11@gmail.com>
2024-02-12 09:55:11 +0700

    Task 1 js day 2

---
+++ Task 1/index.js
@@ -0,0 +1,18 @@
+let favoriteBook1 = 'Kimetsu no Yaiba';
+const favoriteBook2 = 'Solo Leveling';
+
+favoriteBook1 = 'Tokyo Revengers';
+// favoriteBook2 = 'Blue Lock';
+
+console.log(favoriteBook1);
+console.log(favoriteBook2);
+
+let concat = favoriteBook1 + ' & ' + favoriteBook2;
+
+console.log(concat);
+
+if (favoriteBook1 === favoriteBook2) {
+	console.log('true');
+} else {
+	console.log('false');
+}
